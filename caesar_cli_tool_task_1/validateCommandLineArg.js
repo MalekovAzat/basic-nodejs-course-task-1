e35786d6f53ea8918040191bea87d3de0d674ce5
Malekov Azat <malekovAzat@gmail.com>
2021-11-21 22:26:14 +0300

    Definition of done:  - bug fix of validate parameters  - mode caesar_cli to special directory  - add exports where required

--- caesar_cli_tool_task_1/validateCommandLineArg.js
+++ caesar_cli_tool_task_1/validateCommandLineArg.js
@@ -1,53 +1,36 @@
 const fs = require("fs");
 const CustomException = require("./CustomException");
 
-function _onlyOne(argv, reqArgv) {
-  const countOfEachArg = reqArgv.map(
-    (arg) => argv.filter((checkArg) => checkArg === arg).length
-  );
+// count how many times elements from arr2 included in arr1
+function count(arr1, arr2) {
+  let count = 0;
+  // get all elements from arr1 if they in arr2
+  const elements = arr1.filter((elem) => arr2.indexOf(elem) > -1);
 
-  const reducer = (previousValue, currentValue) => previousValue + currentValue;
-  return countOfEachArg.reduce(reducer) === 1;
-}
-
-function _noOrOnlyOne(argv, reqArgv) {
-  const countOfEachArg = reqArgv.map(
-    (arg) => argv.filter((checkArg) => checkArg === arg).length
-  );
-
-  const reducer = (previousValue, currentValue) => previousValue + currentValue;
-  return countOfEachArg.reduce(reducer) <= 1;
-}
-
-// if anleast one elem from reqArgv in arg return true
-function _atLeastOne(argv, reqArgv) {
-  for (let arg of reqArgv) {
-    if (argv.indexOf(arg) !== -1) {
-      return true;
-    }
-  }
-  return false;
+  return elements.length;
 }
 
 function validateArgv(argv) {
-  // check if flags present
-  if (_atLeastOne(argv, [["-c", "--config"]])) {
+  // console.log("!@# argv", count(argv, ["-c", "--config"]) === 0, argv);
+  if (count(argv, ["-c", "--config"]) === 0) {
     throw new CustomException("Provide requirement '-c' or '--config' flag\n");
   }
   // check if only one
-  if (!_onlyOne(argv, ["-c", "--config"])) {
+  if (count(argv, ["-c", "--config"]) > 1) {
     throw new CustomException("Provided too much '-c' or '--config' flags\n");
   }
   // not in the list or only one
-  if (!_noOrOnlyOne(argv, ["-i", "--input"])) {
+  if (count(argv, ["-i", "--input"]) > 1) {
     throw new CustomException("Provided too much '-i' or '--input' flags\n");
   }
   // not in the list or only one
-  if (!_noOrOnlyOne(argv, ["-o", "--output"])) {
+  if (count(argv, ["-o", "--output"]) > 1) {
     throw new CustomException("Provided too much '-0' or '--output' flags\n");
   }
 
-  const configFlagIndex = argv.indexOf("-c") || argv.indexOf("--config");
+  let configFlagIndex = argv.indexOf("-c");
+  configFlagIndex =
+    configFlagIndex > -1 ? configFlagIndex : argv.indexOf("--config");
   if (configFlagIndex > -1) {
     const configRegEx =
       /^((([CR]{1}[01]{1})|([A]{1}))-)*(([CR]{1}[01]{1})|([A]{1}))$/g;
@@ -58,7 +41,8 @@ function validateArgv(argv) {
   }
 
   // check rest args
-  const inputArgIndex = argv.indexOf("-i") || argv.indexOf("--input");
+  let inputArgIndex = argv.indexOf("-i");
+  inputArgIndex = inputArgIndex > -1 ? inputArgIndex : argv.indexOf("--input");
   if (inputArgIndex !== -1) {
     const inputPath = argv[inputArgIndex + 1];
     if (!fs.existsSync(inputPath)) {
@@ -74,7 +58,9 @@ function validateArgv(argv) {
     }
   }
 
-  const ouptutArgIndex = argv.indexOf("-o") || argv.indexOf("--output");
+  let ouptutArgIndex = argv.indexOf("-o");
+  ouptutArgIndex =
+    ouptutArgIndex > -1 ? ouptutArgIndex : argv.indexOf("--output");
   if (ouptutArgIndex !== -1) {
     const ouputPath = argv[ouptutArgIndex + 1];
     if (!fs.existsSync(ouputPath)) {
@@ -82,7 +68,6 @@ function validateArgv(argv) {
         "Provided file path is not exist for output file\n"
       );
     }
-
     try {
       if (!fs.accessSync(ouputPath, fs.constants.W_OK)) {
       }
